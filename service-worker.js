c3df0a54d4899ae02acf1be86cfc48628bb9b0e8
nevillebailey123 <neville.bailey@icloud.com>
2026-08-07 01:48:21 +0000

    Add PWA support

---
+++ service-worker.js
@@ -0,0 +1,111 @@
+const APP_SHELL_CACHE = "building-manager-shell-v1";
+const RUNTIME_CACHE = "building-manager-runtime-v1";
+
+const APP_SHELL_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./storage.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(APP_SHELL_CACHE)
+      .then((cache) => cache.addAll(APP_SHELL_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== APP_SHELL_CACHE && key !== RUNTIME_CACHE)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(networkFirst(event.request, APP_SHELL_CACHE));
+    return;
+  }
+
+  const destination = event.request.destination;
+  if (
+    destination === "script" ||
+    destination === "style" ||
+    destination === "image" ||
+    destination === "font"
+  ) {
+    event.respondWith(staleWhileRevalidate(event.request, RUNTIME_CACHE));
+    return;
+  }
+
+  event.respondWith(networkFirst(event.request, RUNTIME_CACHE));
+});
+
+async function networkFirst(request, cacheName) {
+  const cache = await caches.open(cacheName);
+
+  try {
+    const networkResponse = await fetch(request);
+    cache.put(request, networkResponse.clone());
+    return networkResponse;
+  } catch (_error) {
+    const cachedResponse = await cache.match(request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    if (request.mode === "navigate") {
+      const shellCache = await caches.open(APP_SHELL_CACHE);
+      const shell = await shellCache.match("./index.html");
+      if (shell) {
+        return shell;
+      }
+    }
+
+    throw _error;
+  }
+}
+
+async function staleWhileRevalidate(request, cacheName) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(request);
+
+  const networkPromise = fetch(request)
+    .then((networkResponse) => {
+      cache.put(request, networkResponse.clone());
+      return networkResponse;
+    })
+    .catch(() => null);
+
+  return cachedResponse || networkPromise;
+}
